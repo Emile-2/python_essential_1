38931d9881b3a7ee01b8738ba71b91bf40ee2786
em <emile_thompson@live.co.uk>
2022-03-07 19:48:07 +0000

    cleaned up syntax and variable names

--- exercise_1.py
+++ exercise_1.py
@@ -1,41 +1,40 @@
-#This is app is designed for data entry
-
-no_employees = input("Enter the amount of employees you have: ")
-no_employees_int = int(no_employees)
+number_of_employees = input("Enter the amount of employees you have: ")
+number_of_employees_int = int(number_of_employees)
 ages = []
 employees = 1
 
-while no_employees_int >= employees:
-
+while number_of_employees_int >= employees:
     print(f"\nEnter details for employee {employees}")
+
     first_name = input("Enter employees first name: ")
 
     if first_name.strip():
-        print("\nSuccess")
+        print("Success")
     else:
-        print('\nUnrecognized input, Start over')
+        print(f'\nUnrecognized input, Please enter employee {employees}\'s details again')
         continue
 
     last_name = input("\nEnter employees last name: ")
+
     if last_name.strip():
-        print("\nSuccess")
+        print("Success")
     else:
-        print('\nUnrecognized input, Start over')
+        print(f'\nUnrecognized input, Please enter employee {employees}\'s details again')
         continue
 
-    age = input("\nEnter employees age: ")
+    age = input("Enter employees age: ")
 
     if int(age) >= 18 and int(age) <= 100:
         print("\nSuccess")
         ages.append(int(age))
+
     else:
-        print('\nUnrecognized input, Start over')
+        print(f'\nUnrecognized input, Please enter employee {employees}\'s details again')
         continue
 
     employees += 1
 
     print(f"\n{first_name} {last_name} is {age} years old")
-
 
 if sum(ages) > 500:
     print(f"The sum of all ages is {sum(ages)}")
