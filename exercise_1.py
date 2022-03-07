0e145e543976e80021ff427092af8a266948d4f7
em <emile_thompson@live.co.uk>
2022-03-07 19:17:42 +0000

    Changed for loop to while loop

--- exercise_1.py
+++ exercise_1.py
@@ -1,29 +1,24 @@
 #This is app is designed for data entry
 
-
 no_employees = input("Enter the amount of employees you have: ")
-
-
+no_employees_int = int(no_employees)
 ages = []
+employees = 0
 
+while no_employees_int >= employees:
 
-for index in range(int(no_employees)):
-
-    print(f"Enter details for employee {index + 1}")
+    print(f"Enter details for employee {employees + 1}")
     first_name = input("Enter employees first name: ")
 
     if first_name.strip():
         print("Success")
-
     else:
         print('Unrecognized input')
         continue
 
-
     last_name = input("Enter employees last name")
     if last_name.strip():
         print("Success")
-
     else:
         print('Unrecognized input')
         continue
@@ -33,10 +28,11 @@ for index in range(int(no_employees)):
     if int(age) >= 18 and int(age) <= 100:
         print("Success")
         ages.append(int(age))
-
     else:
         print('Unrecognized input')
         continue
+
+    employees += 1
 
     print(f"{first_name} {last_name} is {age} years old")
 
