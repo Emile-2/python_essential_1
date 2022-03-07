5ad296bb36703693d357885d09a169319cb71b81
em <emile_thompson@live.co.uk>
2022-03-07 19:33:41 +0000

    changed the sum of ages to only print when more than 500

--- exercise_1.py
+++ exercise_1.py
@@ -3,40 +3,42 @@
 no_employees = input("Enter the amount of employees you have: ")
 no_employees_int = int(no_employees)
 ages = []
-employees = 0
+employees = 1
 
 while no_employees_int >= employees:
 
-    print(f"Enter details for employee {employees + 1}")
+    print(f"\nEnter details for employee {employees}")
     first_name = input("Enter employees first name: ")
 
     if first_name.strip():
-        print("Success")
+        print("\nSuccess")
     else:
-        print('Unrecognized input')
+        print('\nUnrecognized input, Start over')
         continue
 
-    last_name = input("Enter employees last name")
+    last_name = input("\nEnter employees last name: ")
     if last_name.strip():
-        print("Success")
+        print("\nSuccess")
     else:
-        print('Unrecognized input')
+        print('\nUnrecognized input, Start over')
         continue
 
-    age = input("Enter employees age")
+    age = input("\nEnter employees age: ")
 
     if int(age) >= 18 and int(age) <= 100:
-        print("Success")
+        print("\nSuccess")
         ages.append(int(age))
     else:
-        print('Unrecognized input')
+        print('\nUnrecognized input, Start over')
         continue
 
     employees += 1
 
-    print(f"{first_name} {last_name} is {age} years old")
+    print(f"\n{first_name} {last_name} is {age} years old")
 
-print(sum(ages))
+
+if sum(ages) > 500:
+    print(f"The sum of all ages is {sum(ages)}")
 
 
 
