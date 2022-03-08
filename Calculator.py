60c84d1ecde81595259606549aa99ebaa2fd0a9b
em <emile_thompson@live.co.uk>
2022-03-08 15:50:06 +0000

    added feature to stop error if user puts in non-digits

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,11 @@
-# ask user for input of two integers
-
 # create operations
 def operation_list():
-    print("""1. Add
-          2. Subtract
-          3. Multiply
-          4. Divide
-          5. Exit
+    print("""
+    1. Add
+    2. Subtract
+    3. Multiply
+    4. Divide
+    5. Exit
           """)
 
 def add(number_one, number_two):
@@ -21,24 +20,26 @@ def multiply(number_one, number_two):
 def divide(number_one, number_two):
     return number_one / number_two
 
-
-#get input for numbers
-first_number_str = input("Please enter first number: ")
-second_number_str = input("please enter second number")
-
-#get input for operation selected
-
-
-first_number = int(first_number_str)
-second_number = int(second_number_str)
-
-
 if __name__ =='__main__':
+
     while True:
+        #get input for numbers
+        first_number_str = input("Please enter first number: ")
+        second_number_str = input("please enter second number")
+        if first_number_str.isdigit() and second_number_str.isdigit():
+
+            first_number = int(first_number_str)
+            second_number = int(second_number_str)
+            break
+
+    while True:
+        # get input for operation selected
         operation_list()
         #i orginally forgot to cast this input to an int, causing issues
         operation_str = input("Please select which operation")
-        operation = int(operation_str)
+        if operation_str.isdigit():
+            operation = int(operation_str)
+        # operation = int(operation_str)
 
         if operation == 1:
             print(add(first_number, second_number))
@@ -59,6 +60,9 @@ if __name__ =='__main__':
         elif operation == 5:
             print("Thanks for using the calculator")
             break
+
+        else:
+            print("Unknown input")
 
 
 
