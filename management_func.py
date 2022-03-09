e9a9cce69e0ea58159b0606d26fb77dcd42f3bbd
em <emile_thompson@live.co.uk>
2022-03-09 11:19:26 +0000

    created basic functions for user input

---
+++ management_func.py
@@ -0,0 +1,72 @@
+#employee id
+
+def emplyee_id():
+    while True:
+        id = input("Please enter the employee ID: ")
+        if id.isdigit():
+            id = int(id)
+            return id
+
+
+#functions for name entry
+def name_entry(text):
+    while True:
+        name = input(f"Please enter {text} name")
+        if len(name.strip()) > 1:
+            return name
+
+#birth year
+def birth_year():
+    while True:
+        date = input("Please enter the birthyear:")
+        if date.isdigit():
+            date = int(date)
+            if 1900 < date < 2004:
+                return int(date)
+
+#birth month
+def birth_month():
+    while True:
+        date = input("Please enter the birth month:")
+        if date.isdigit():
+            date = int(date)
+            if date in range(1, 13):
+                return int(date)
+
+
+#birth day
+def birth_month():
+    while True:
+        date = input("Please enter the day of birth:")
+        if date.isdigit():
+            date = int(date)
+            if date in range(1, 32):
+                return int(date)
+
+#position
+def position():
+    while True:
+        name = input(f"Please enter position of employee")
+        return name
+
+#whether graduated from uni (boolean)
+def uni():
+    while True:
+        graduate = input("Did the employee graduate from uni? y/n")
+        if graduate.isalpha() and len(graduate) == 1:
+            if graduate.capitalize() == "Y":
+                return True
+            elif graduate.capitalize() == "N":
+                return False
+            else:
+                continue
+
+#create a dictionary
+
+#populate dictionary with dictionary of values
+
+students = {}
+
+
+
+
